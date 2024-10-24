784a286529fabe4fbec4d2ad22a7d7ce81c5593b
Dima Melnyk <dimam4659@gmail.com>
2024-10-24 21:47:00 +0300

    Task_2_lesson_2

--- Lesson_2/Task_2/Task_2.cpp
+++ Lesson_2/Task_2/Task_2.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+    vector<int> values(5);
 
+    //zaput cusel v korustuvaca
+    for (int i = 0; i < 5; ++i) {
+        cout << "Enter " << (i + 1) << " value: ";
+        cin >> values[i];
+    }
+    for (int i = 0; i < 5; ++i) {
+        int stars = values[i];
+        for (int j = 0; j < stars; ++j) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
