9397b8b942735a1c04f9a863a842c46d5c15ea4b
Dima Melnyk <dimam4659@gmail.com>
2024-10-24 20:56:37 +0300

    Task_44

--- Lesson_1/Task_4/Task_4.cpp
+++ Lesson_1/Task_4/Task_4.cpp
@@ -7,7 +7,20 @@ int main() {
     cout << "vvedit vashu zarplatu: ";
     cin >> salary;
 
-    cout << ((salary - 1000 < 0) ? "nada rabotat bilshe" :
-                 ((salary - 1000000 < 0) ? "Ti molodec" : "Ti milioner:3")) << endl;
+    // Перевірка для зарплати менше тисячі
+    if (salary - 1000 < 0) {
+        cout << "nada rabotat bilshe" << endl;
+        return 0;
+    }
+
+    // Перевірка для зарплати між тисячею і мільйоном
+    if (salary - 1000000 < 0) {
+        cout << "Ti molodec" << endl;
+        return 0;
+    }
+
+    // Якщо зарплата більше мільйона
+    cout << "Ti milioner:3" << endl;
+
     return 0;
 }
