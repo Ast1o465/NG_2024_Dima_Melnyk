137019b1f0b6188b90a4e614f89c00f3ee09d47a
Ast1o465 <dimam4659@gmail.com>
2024-12-29 23:05:40 +0200

    removed variables with one character

--- Lesson_2/Task_2/Task_2.cpp
+++ Lesson_2/Task_2/Task_2.cpp
@@ -7,17 +7,17 @@ int main() {
     int values[5];
     int sortedValues[5];
 
-    for (int i = 0; i < 5; i++) {
-        cout << "Enter " << (i + 1) << " value: ";
-        cin >> values[i];
-        sortedValues[i] = values[i];
+    for (int index = 0; index < 5; index++) {
+        cout << "Enter " << (index + 1) << " value: ";
+        cin >> values[index];
+        sortedValues[index] = values[index];
     }
 
     sort(sortedValues, sortedValues + 5, greater<int>());
 
     for (int row = 0; row < sortedValues[0]; row++) {
-        for (int col = 0; col < 5; col++) {
-            if (row < values[col]) {
+        for (int column = 0; column < 5; column++) {
+            if (row < values[column]) {
                 cout << "* ";
             } else {
                 cout << "  ";
